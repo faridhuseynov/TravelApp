017d3bb55a86690c3317e96e87ef1a21bf0307da
Farid Huseynov <F.H189@hotmail.com>
2019-01-26 00:27:47 +0400

    RouteMap view and viewmodel added. message added, should be checked

--- TravelApp/ViewModels/RouteMapViewModel.cs
+++ TravelApp/ViewModels/RouteMapViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelApp.Messages;
 using TravelApp.Models;
 using TravelApp.Services;
 
@@ -14,12 +18,37 @@ namespace TravelApp.ViewModels
         private readonly INavigationService navigation;
         private readonly AppDbContext db;
 
+        private ICollection<Location> locations;
+        public ICollection<Location> Locations { get => locations; set => Set(ref locations, value); }
+
+
         public RouteMapViewModel(INavigationService navigation,AppDbContext db)
         {
             this.navigation = navigation;
             this.db = db;
 
-            
+            Messenger.Default.Register<MapReviewMessage>(this,msg=>
+            {
+                foreach (var item in msg.Destinations)
+                {
+                    Locations.Add(new Location
+                    {
+                        Latitude = double.Parse(item.Latitude),
+                        Longitude = double.Parse(item.Longitude)
+                    });
+                }
+            });
+        }
+
+        private RelayCommand backCommand;
+        public RelayCommand BackCommand
+        {
+            get => backCommand ?? (backCommand = new RelayCommand(
+                () =>
+                {                    
+                    navigation.Navigate<ReviewTripViewModel>();
+                }
+            ));
         }
     }
 }
